c9075753a25a39a297907a3904fc753b08f7f426
Patrik Lundin <patlu@sunet.se>
2025-10-25 13:14:58 +0200

    Improve configuration management
    * Modify file watchers when an updated config file points to new
      filenames. Previously what file to watch was only registered at
      startup.
    * Log a warning message if a config setting that requires a restart has been
      modified. Introduce a "reload" struct tag to mark which settings are
      dynamically picked up.
    * Signal minimiser workers and the histogram sender that configurations
      has been updated so they can refresh their local config.
    * Add a mutex around aggreggSender now that it might be modified at
      runtime.
    * Make more functions use edm.getConfig() instead of passing in settings
      when they are called.
    * When modifying the config file with an editor in-place rather than
      doing atomic renames with a new config file I noticed an issue where
      viper.UnmarshalExact sometimes served stale config settings.
      Now we call viper.ReadInConfig() explicitly after deduping file
      events.
    * Drop edm.histogramSenderDisabled and edm.mqttDisabled since we can
      check the config fields directly instead.
    * Add some more logging.
    * Update tests to reflect new config usage.
    
    Additional tweaks:
    * aggregateSender lives in the edm struct but also contained an edm
      field itself which seemed redundant. The edm field was only used for
      logging so only store the logger instead.

--- pkg/runner/mqtt.go
+++ pkg/runner/mqtt.go
@@ -47,7 +47,7 @@ func (pel pahoErrorLogger) Printf(format string, v ...interface{}) {
 	pel.logger.Error(fmt.Sprintf(format, v...))
 }
 
-func (edm *dnstapMinimiser) newAutoPahoClientConfig(caCertPool *x509.CertPool, server string, clientID string, clientCertStore *certStore, mqttKeepAlive uint16, localFileQueue *file.Queue) (autopaho.ClientConfig, error) {
+func (edm *dnstapMinimiser) newAutoPahoClientConfig(caCertPool *x509.CertPool, server string, clientID string, mqttKeepAlive uint16, localFileQueue *file.Queue) (autopaho.ClientConfig, error) {
 	u, err := url.Parse(server)
 	if err != nil {
 		return autopaho.ClientConfig{}, fmt.Errorf("newAutoPahoClientConfig: unable to parse URL: %w", err)
@@ -57,7 +57,7 @@ func (edm *dnstapMinimiser) newAutoPahoClientConfig(caCertPool *x509.CertPool, s
 		ServerUrls: []*url.URL{u},
 		TlsCfg: &tls.Config{
 			RootCAs:              caCertPool,
-			GetClientCertificate: clientCertStore.getClientCertificate,
+			GetClientCertificate: edm.mqttClientCertStore.getClientCertificate,
 			MinVersion:           tls.VersionTLS13,
 		},
 		KeepAlive:      mqttKeepAlive,
