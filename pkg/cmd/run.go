9bfeaee79e929291f0f35f17fec34c2d92e3e77f
Mattias Jacobsson <code@kryptogram.se>
2026-09-11 14:16:13 +0200

    pkg/runner: switch to an in memory ring buffer as MQTT queue

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -30,7 +30,6 @@ func newRunFlagSet(conf *runner.Config) (fs *flag.FlagSet) {
 	fs.BoolVar(&conf.DisableSessionFiles, "disable-session-files", conf.DisableSessionFiles, "do not write out session parquet files")
 	fs.BoolVar(&conf.DisableHistogramSender, "disable-histogram-sender", conf.DisableHistogramSender, "do not check for histogram files to upload to core")
 	fs.BoolVar(&conf.DisableMQTT, "disable-mqtt", conf.DisableMQTT, "disable MQTT message sending")
-	fs.BoolVar(&conf.DisableMQTTFilequeue, "disable-mqtt-filequeue", conf.DisableMQTTFilequeue, "disable MQTT file based queue")
 	fs.BoolVar(&conf.EnableManualParquetRotation, "enable-manual-parquet-rotation", conf.EnableManualParquetRotation, "enable localhost HTTP endpoint for manually rotating session and histogram parquet files")
 	fs.BoolVar(&conf.PebbleSync, "pebble-sync", conf.PebbleSync, "fsync seen-qname pebble writes")
 
@@ -136,8 +135,6 @@ func overrideFor(name string, src *runner.Config) runner.ConfigOverride {
 		return func(c *runner.Config) { c.DisableHistogramSender = src.DisableHistogramSender }
 	case "disable-mqtt":
 		return func(c *runner.Config) { c.DisableMQTT = src.DisableMQTT }
-	case "disable-mqtt-filequeue":
-		return func(c *runner.Config) { c.DisableMQTTFilequeue = src.DisableMQTTFilequeue }
 	case "enable-manual-parquet-rotation":
 		return func(c *runner.Config) { c.EnableManualParquetRotation = src.EnableManualParquetRotation }
 	case "pebble-sync":
